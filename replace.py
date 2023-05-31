e647b1f424d793e7d5e467af33d37115f2e9c287
ossims <113637544+ossims@users.noreply.github.com>
2023-05-31 17:59:45 -0400

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,8 @@
+def word_replace():
+
+    str = "Hi guys, I am Tomi, and hi hi hi"
+    word_to_replace = input("Enter the word to replace")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+word_replace()
